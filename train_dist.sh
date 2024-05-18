917b5ec7c995a90c456a3da3a5f60f554427456f
lhurrr <limhur.ng@gmail.com>
2024-05-18 16:56:23 +0800

    add grounding dino custom tune codes

---
+++ train_dist.sh
@@ -0,0 +1,28 @@
+GPU_NUM=$1
+CFG=$2
+DATASETS=$3
+OUTPUT_DIR=$4
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+# Change ``pretrain_model_path`` to use a different pretrain. 
+# (e.g. GroundingDINO pretrain, DINO pretrain, Swin Transformer pretrain.)
+# If you don't want to use any pretrained model, just ignore this parameter.
+
+python3 main.py \
+        --output_dir ${OUTPUT_DIR} \
+        -c ${CFG} \
+        --datasets ${DATASETS}  \
+        --pretrain_model_path groundingdino_swint_ogc.pth \
+        --options text_encoder_type=bert-base-uncased \
+        --resume logs/checkpoint0001.pth
+        
+        
+
+
+
+# python3 tools/inference_on_a_image.py   -c tools/GroundingDINO_SwinT_OGC.py   -p logs/checkpoint0000.pth   -i ../til-ai-24-advanced/images/image_0.jpg   -t "grey missile . red, white, and blue light aircraft . green and black missile . white and red helicopter ."   -o output
+
+# bash train_dist.sh 1 config/cfg_odvg.py config/datasets_od_example.json logs 
